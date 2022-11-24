4341ec69c36e9a33eb14ac8e62e449af640c484a
Aleksandra Hrybik <aleksandra.hrybik98@wp.pl>
2022-11-24 15:49:27 +0100

    change language

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-let liczba = Math.floor(Math.random() * 100 + 1);
+// Próba dodania informacji o ilości prób
 
-let proba = 0;
+/*let counter = 0;
+
+const el = document.createElement("div");
+
+const div = document.querySelector(".proby");
+
+el.classList.add("element");
+
+el.innerText = ++counter;
+
+div.appendChild(el); */
+
+let randomNr = Math.floor(Math.random() * 100 + 1);
+
+let shot = 0;
+
+let hit = false;
+
+const mistake = document.getElementById("blad");
 
 const result = document.getElementById("result");
 
@@ -12,20 +30,25 @@ btn.addEventListener("click", function () {
 });
 
 function check(nr) {
-  if (nr >= 1 && nr <= 100) {
+  if (nr >= 1 && nr <= 100 && nr == Math.floor(nr)) {
     game(nr);
+    mistake.innerHTML = " ";
+    document.getElementById("number").style.border = "3px solid black";
+  } else if (nr !== Math.floor(nr) && hit == false) {
+    mistake.innerHTML = "Wpisz liczbę naturalną";
+    document.getElementById("number").style.border = "3px solid red";
   } else {
-    result.innerHTML = "Liczba spoza zakresu";
+    mistake.innerHTML = "Liczba spoza zakresu";
   }
 }
 
 function game(i) {
-  proba++;
-  if (i == liczba) {
-    result.innerHTML = "Udało Ci się za " + proba + " razem";
-  } else if (i > liczba) {
-    result.innerHTML = proba + "." + " Za dużo";
+  shot++;
+  if (i == randomNr) {
+    result.innerHTML = "Udało Ci się za " + shot + " razem";
+  } else if (i > randomNr) {
+    result.innerHTML = shot + "." + " Za dużo";
   } else {
-    result.innerHTML = proba + "." + " Za mało";
+    result.innerHTML = shot + "." + " Za mało";
   }
 }
